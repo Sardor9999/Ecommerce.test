6d0a9cb8afa66f441e6432002bdb3d8e9f8e3a6c
Sardor9999 <sardoryuldashov9999@gmail.com>
2023-05-26 10:15:24 +0300

    yangilandi

--- README.md
+++
@@ -1,2 +0,0 @@
-# Ecommerce.test
-Bu kod Ecommerce bilan ishlash ko'nikmasini oshirish uchun
--- api/apps.py
+++
@@ -1,6 +0,0 @@
-from django import AppConfig
-
-
-class ApiConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'api'
